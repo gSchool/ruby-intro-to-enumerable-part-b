ceac4ca22960f4d1562bfb2d0a3c351341b280e8
lmnleaf <larkfleming@gmail.com>
2014-12-16 10:48:13 -0700

    Exercise 3

--- exercises/03_select.rb
+++ exercises/03_select.rb
@@ -1,6 +1,19 @@
 def select(items, &block)
+
+  result = []
+
+  items.each do |item|
+    if block.call(item)
+      result << item
+    end
+  end
+
+  result
+
 end
 
+# Benefit - anytime someone wants to filter an array they can use this code
+# with a block that says how they want to filter the array
 # ------ code above this line ------
 
 require 'rspec/autorun'
@@ -9,6 +22,7 @@ RSpec.describe '#select' do
   it 'returns an array of words shorter than the given length' do
     input = ['a', 'big', 'black', 'bear', 'is', 'running']
 
+# Here's the items and block (following the select and before .to)
     expect(select(input) { |word| word.length < 3}).to eq(['a', 'is'])
     expect(select(input) { |word| word.length < 4}).to eq(['a', 'big', 'is'])
   end
